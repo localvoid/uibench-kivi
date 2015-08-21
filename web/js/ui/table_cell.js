d7eca7584408f452522abce741dd9829e14a19b3
Boris Kaul <localvoid@gmail.com>
2015-08-21 10:45:50 +0600

    Improve onclick handler performance

--- web/js/ui/table_cell.js
+++ web/js/ui/table_cell.js
@@ -7,10 +7,15 @@ app.ui.table_cell.d.tag = 'td';
 
 /** @param {!kivi.Component<string, null>} c */
 app.ui.table_cell.d.init = function(c) {
-  c.element.onclick = function(e) {
-    console.log('Click', c.data);
-    e.stopPropagation();
-  };
+  c.element._xtag = c;
+  c.element.onclick = app.ui.table_cell._handleClick;
+};
+
+/** @param {Event} e */
+app.ui.table_cell._handleClick = function(e) {
+  var currentTarget = /** @type {{_xtag:!kivi.Component<string, null>}} */(e.currentTarget);
+  console.log('Click', currentTarget._xtag.data);
+  e.stopPropagation();
 };
 
 /** @param {!kivi.Component<string, null>} c */
